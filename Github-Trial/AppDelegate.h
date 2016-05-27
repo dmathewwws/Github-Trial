a5291cf0cf37a8302c7fafedc5a7f52e31942559
Daniel Mathews <dannyl.mathews@gmail.com>
2016-05-27 16:13:27 -0700

    Initial Commit

---
+++ Github-Trial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github-Trial
+//
+//  Created by Daniel Mathews on 2016-05-27.
+//  Copyright © 2016 Daniel Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github-Trial/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Github-Trial
+//
+//  Created by Daniel Mathews on 2016-05-27.
+//  Copyright © 2016 Daniel Mathews. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
